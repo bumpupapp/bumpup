28088cf3a30f9d23eb756e8d35f86d86d4c312ea
Daniel Richter <danielrichter@posteo.de>
2022-12-14 06:42:30 +0100

    fix ci
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- ci/copy.ts
+++ ci/copy.ts
@@ -1,15 +1,17 @@
-import {walk} from "https://deno.land/std@0.165.0/fs/walk.ts";
-import {globToRegExp} from "https://deno.land/std@0.158.0/path/glob.ts";
 import toArray from "https://esm.sh/@async-generators/to-array@0.1.0"
 import * as path from "https://deno.land/std@0.167.0/path/mod.ts";
-import './time.ts'
+import { ensureFile, expandGlob } from "https://deno.land/std@0.167.0/fs/mod.ts";
 
 const copy = async ({files, outdir}) => {
     const common = path.common(files)
     const res = await Promise.allSettled(files
-        .map(file=>([file, path.join(outdir,file.replace(common,''))]))
-        .map(([from, to])=>Deno.mkdir(path.dirname(to), {recursive: true}).then(Deno.copyFile(from,to)).then(()=>[from, to]))
-    )
+        .map(file=>([file, path.join(Deno.cwd(),outdir,file.replace(common,''))]))
+        .map(async([from, to])=>{
+            await ensureFile(to)
+            await Deno.copyFile(from,to)
+            return [from, to]
+        }))
+
     const status = res.map(({status, reason, value})=>{
         if(status === "fulfilled"){
             const [from, to] = value
@@ -22,6 +24,5 @@ const copy = async ({files, outdir}) => {
 
 }
 const outdir = 'build'
-const files = (await toArray(walk('.', {match: ['packages/*/**/*.*'].map(globToRegExp)}))).map(entry=>entry.path)
-
+const files = (await toArray(expandGlob('packages/*/**/*.*'))).map(entry=>entry.path)
 await copy({files, outdir})
